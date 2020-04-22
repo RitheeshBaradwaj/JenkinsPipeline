0459ac73db1226a59491bcaf4110b2d655761134
Ritheesh <bunnyrb4@gmail.com>
2020-04-22 11:34:44 +0530

    edited Artifactory repository name

--- build_script.ps1
+++ build_script.ps1
@@ -1,4 +1,4 @@
-﻿$zip_folder = mkdir ("build" + " " +($(get-date -f MM-dd-yyyy-HH-mm-ss)))
+$zip_folder = mkdir ("build" + " " +($(get-date -f MM-dd-yyyy-HH-mm-ss)))
 Move-Item -Path "dist/app.exe" -Destination $zip_folder
 Compress-Archive -Path $zip_folder -DestinationPath $zip_folder
-jfrog rt u "*.zip" generic-repo
+jfrog rt u "build*.zip" generic-local
