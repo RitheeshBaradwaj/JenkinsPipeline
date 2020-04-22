7cefba78aecf930360dd133b1a456c9aaea8566c
Ritheesh <bunnyrb4@gmail.com>
2020-04-22 11:17:44 +0530

    Added a script to include versions

---
+++ build_script.ps1
@@ -0,0 +1,4 @@
+﻿$zip_folder = mkdir ("build" + " " +($(get-date -f MM-dd-yyyy-HH-mm-ss)))
+Move-Item -Path "dist/app.exe" -Destination $zip_folder
+Compress-Archive -Path $zip_folder -DestinationPath $zip_folder
+jfrog rt u "*.zip" generic-repo
